5bd49d797dd6bcebe46687e3ca8667929f4ea647
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-26 09:47:08 -0800

    Upgraded the library to use promises

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http = new HTTPLib();
 
-// // Get posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//     if(err) {
-//         console.log(err);
-//     } else {
-//         console.log(posts);
-//     }
-// });
+// Get posts
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(posts);
+    }
+});
 
 // // Get single post
 // http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
@@ -18,11 +18,11 @@ const http = new HTTPLib();
 //     }
 // });
 
-// Create data
-const data = {
-    title : 'Custom Post',
-    body : 'Custom post body.'
-};
+// // Create data
+// const data = {
+//     title : 'Custom Post',
+//     body : 'Custom post body.'
+// };
 
 // // Create post
 // http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
@@ -42,11 +42,11 @@ const data = {
 //     }
 // });
 
-// Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(response);
-    }
-});
+// // Delete Post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(response);
+//     }
+// });
