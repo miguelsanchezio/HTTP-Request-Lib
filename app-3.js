aef4aa5ab73f35ddd602ca1d93a99b07428e2c49
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-26 10:28:35 -0800

    Upgraded the library to use async and await

---
+++ app-3.js
@@ -0,0 +1,28 @@
+const http = new HTTPlib;
+
+// // Get users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// User data
+const data = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'jdoe@gmail.com'
+}
+
+// // Create post
+// http.post('http://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// // Update post
+// http.put('http://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+http.delete('http://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
